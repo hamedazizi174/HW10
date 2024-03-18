8a86178255725d8d3c6bceb109693bf97994762d
hamed <hamedazizi1374@gmail.com>
2024-03-18 23:07:15 +0330

    create show and close modal

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 "use strict";
 
+const addModal = document.getElementById("addModal");
+const overlay = document.getElementById("overlay");
+
+function showAddModal() {
+  addModal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  addModal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
 jalaliDatepicker.startWatch({
   minDate: "attr",
 });
 
-let Deadline = document.getElementById("Deadline");
+const Deadline = document.getElementById("Deadline");
+
 Deadline.addEventListener("click", () => {
   jalaliDatepicker.show(Deadline);
 });
