8ce8c5a7acbaa439ded1381d385cfb559e0b4495
hamed <hamedazizi1374@gmail.com>
2024-03-18 22:32:54 +0330

    create add modal

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+jalaliDatepicker.startWatch({
+  minDate: "attr",
+});
+
+let Deadline = document.getElementById("Deadline");
+Deadline.addEventListener("click", () => {
+  jalaliDatepicker.show(Deadline);
+});
