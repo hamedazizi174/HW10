62929ae37cc9d89154c266cced958416962825a1
hamed <hamedazizi1374@gmail.com>
2024-03-19 18:43:16 +0330

    add data to local storage

--- script.js
+++ script.js
@@ -2,23 +2,42 @@
 
 const addModal = document.getElementById("addModal");
 const overlay = document.getElementById("overlay");
+const addForm = document.getElementById("addForm");
+const Deadline = document.getElementById("Deadline");
+
+const storagedToDos = "To Dos";
+
+const toDos = JSON.parse(localStorage.getItem(storagedToDos)) || [];
 
 function showAddModal() {
   addModal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 }
 
-function closeModal() {
+function closeAddModal() {
   addModal.classList.add("hidden");
   overlay.classList.add("hidden");
+}
+
+function addDataToLocalStorage(event) {
+  event.preventDefault();
+  const { taskName, priority, deadline } = event.target;
+  const toDo = {
+    taskName: taskName.value,
+    priority: priority.value,
+    status: "To Do",
+    deadline: deadline.value,
+  };
+  toDos.push(toDo);
+  localStorage.setItem(storagedToDos, JSON.stringify(toDos));
 }
 
 jalaliDatepicker.startWatch({
   minDate: "attr",
 });
 
-const Deadline = document.getElementById("Deadline");
-
 Deadline.addEventListener("click", () => {
   jalaliDatepicker.show(Deadline);
 });
+
+addForm.addEventListener("submit", addDataToLocalStorage);
